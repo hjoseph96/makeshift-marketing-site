f916fac967830a2e922a4f47b803cc70954e6a0e
Herbert Joseph <techniquea2z@gmail.com>
2026-06-05 11:31:50 -0400

    fix(<title>): fit title tag

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -20,7 +20,7 @@ export interface MetaDataConfig extends Omit<MetaData, 'title'> {
 
 const config = siteConfig as any;
 
-const DEFAULT_SITE_NAME = 'Hexborn | Indie Action RPG';
+const DEFAULT_SITE_NAME = 'Makeshift Software Inc.';
 
 const getSite = () => {
   const _default = {
